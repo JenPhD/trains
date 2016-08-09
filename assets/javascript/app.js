448e4faa66e6773e0620485b418cb986b11e443c
JenPhD <jpemberton76@gmail.com>
2016-08-09 15:31:22 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+var config = {
+    apiKey: "AIzaSyDzUZXXHY05Sdeif56QvlPOGP8B5RCq5pI",
+    authDomain: "trains-3ce1d.firebaseapp.com",
+    databaseURL: "https://trains-3ce1d.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
